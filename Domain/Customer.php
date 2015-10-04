2c97f624512a8731649dee35b18f32b7c83c2083
Faizel Jabaar <faizeljabaar@gmail.com>
2015-10-04 22:56:45 +0200

    [Faizel] - Customer class done, with tests working. Still working on login and shopping cart.

--- Domain/Customer.php
+++ Domain/Customer.php
@@ -6,21 +6,53 @@
         private $name;
         private $surname;
         private $address;
+        private $email;
+        private $password;
         private $bank;
         private $card_no;
         
-        /**
-         * 
-         * @assert (1, param1) == 1
-         */
-        function __construct($id, $name, $surname, $address, $bank, $card_no) {
+        public function __construct($id, $name, $surname, $address, $email, $password, $bank, $card_no) {
             
             $this->id = $id;
             $this->name = $name;
             $this->surname = $surname;
             $this->address = $address;
+            $this->email = $email;
+            $this->password = $password;
             $this->bank = $bank;
             $this->card_no = $card_no;
+        }
+        
+        public function getID() {
+            return $this->id;
+        }
+        
+        public function getName() {
+            return $this->name;
+        }
+        
+        public function getSurname() {
+            return $this->surname;
+        }
+        
+        public function getAddress() {
+            return $this->address;
+        }
+        
+        public function getEmail() {
+            return $this->email;
+        }
+        
+        public function getPassword() {
+            return $this->password;
+        }
+        
+        public function getBank() {
+            return $this->bank;
+        }
+        
+        public function getCardNo() {
+            return $this->card_no;
         }
     }
 ?>
---
+++ test/Domain/LoginTest.php
@@ -0,0 +1,12 @@
+<?php
+    
+    require_once('C:/xampp/htdocs/ip3_PHP_project/Domain/Login.php');
+
+    class LoginTest extends PHPUnit_Framework_TestCase {
+
+        public function testLogin() {
+            
+            //TEST LOGIN
+        }
+    }
+?>
